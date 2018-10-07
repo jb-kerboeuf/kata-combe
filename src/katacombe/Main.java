3b46cc508c302e79a8a7bda3d2471b0c8173c5b5
Jean-Baptiste <jean-baptiste.kerboeuf@johnpaul.com>
2018-10-07 15:33:48 +0200

    create mapper for DrinkMaker

--- src/katacombe/Main.java
+++ src/katacombe/Main.java
@@ -12,4 +12,33 @@ public class Main {
     public static void main(String[] args) {
     }
     
+    public static String mapForDrinkMaker(Instruction in) {
+        String drinkMakerString;
+        
+        if (in.message != null) {
+            drinkMakerString = "M:" + in.message;
+            return drinkMakerString;
+        }
+        
+        switch (in.drink) {
+            case "tea": drinkMakerString = "T";
+                break;
+            case "chocolate": drinkMakerString = "H";
+                break;
+            case "coffee": drinkMakerString = "C";
+                break;
+            default: drinkMakerString = "M:Error";
+        }
+        switch (in.sugar) {
+            case 0: drinkMakerString += "::";
+                break;
+            case 1: drinkMakerString += ":1:0";
+                break;
+            case 2: drinkMakerString += ":2:0";
+                break;
+            default: drinkMakerString = "M:Error";
+        }
+        
+        return drinkMakerString;
+    }
 }
