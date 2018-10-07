9c6439d3f74433b2b3907fd152efa7199576e621
Jean-Baptiste <jean-baptiste.kerboeuf@johnpaul.com>
2018-10-07 15:56:44 +0200

    create class Drink

---
+++ src/katacombe/Drink.java
@@ -0,0 +1,16 @@
+package katacombe;
+
+/**
+ *
+ * @author JB Kerboeuf
+ */
+public class Drink {
+    public final String name;
+    public final double price;
+
+    public Drink(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+    
+}
